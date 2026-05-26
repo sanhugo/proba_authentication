b058c553a7dbefbcd8c14df8f54a0753172de55c
sanhugo <sanhugo4u@hotmail.com>
2026-05-27 03:52:35 +0800

    Ready first version of OpenAPI-specification

--- src/main/java/ru/proba/authentication/enums/Role.java
+++ src/main/java/ru/proba/authentication/enums/Role.java
@@ -5,8 +5,7 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     SUPERADMIN, //владелец системы
     ADMIN, //администратор системы
-    USER, //пользователь (студент),
-    PARENT, //родитель
+    USER, //пользователь (студент)
     MODERATOR, //модератор пробника
     REFEREE, //эксперт
     OWNER; //владелец помещения
