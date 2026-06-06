3d1b388b0e77a31255958d13781b843057d5788b
sanhugo <sanhugo4u@hotmail.com>
2026-06-06 15:17:26 +0800

    Added OpenAPI generation + Redis for cache

--- src/main/java/ru/proba/authentication/config/SecurityConfig.java
+++ src/main/java/ru/proba/authentication/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package ru.proba.authentication.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
--- src/main/java/ru/proba/authentication/service/AuthService.java
+++ src/main/java/ru/proba/authentication/service/AuthService.java
@@ -1,13 +1,23 @@
 package ru.proba.authentication.service;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.jspecify.annotations.NullMarked;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.proba.authentication.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AuthService implements UserDetailsService {
+    UserRepository userRepository;
+
     @Override
+    @NullMarked
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
     }
