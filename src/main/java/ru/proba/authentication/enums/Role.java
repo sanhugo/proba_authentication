bfa51f29086e6c8daa3cad334926294532de0132
sanhugo <sanhugo4u@hotmail.com>
2026-06-17 00:58:53 +0800

    added new role

--- src/main/java/ru/proba/authentication/enums/Role.java
+++ src/main/java/ru/proba/authentication/enums/Role.java
@@ -5,7 +5,8 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     SUPERADMIN, //владелец системы
     ADMIN, //администратор системы
-    USER, //пользователь (студент)
+    USER, //пользователь (новый)
+    STUDENT, //студент
     MODERATOR, //модератор пробника
     REFEREE, //эксперт
     OWNER; //владелец помещения
